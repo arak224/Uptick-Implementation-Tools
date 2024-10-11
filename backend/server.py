4db2507613a50e253ce319df02488721f1222727
arak224 <austinrakowski@gmail.com>
2024-10-11 08:12:38 -0600

    woah

--- backend/server.py
+++ backend/server.py
@@ -10,6 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://austinrakowski224:SkoDucks
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+test
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
